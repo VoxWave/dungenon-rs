210974300d1353830886109b458725b58995c59c
Victor Bankowski <victor_bankowski@hotmail.com>
2016-04-15 23:46:16 +0300

    started implementing roomgen

--- src/generator/room.rs
+++ src/generator/room.rs
@@ -4,5 +4,9 @@ use rand::Rand;
 use rand::Rng;
 
 pub struct RoomGen {
-    rand: XorShiftRng,
+    rand_x: XorShiftRng,
+    rand_y: XorShiftRng,
+    max_room_size: usize,
+    min_room_size: usize,
+    attempts: u64,
 }
