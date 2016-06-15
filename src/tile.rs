a6bd85b6ac6362577237ebb9a658a049805cc6bd
Victor Bankowski <victor_bankowski@hotmail.com>
2016-06-15 20:01:42 +0300

    Faction tile added

--- src/tile.rs
+++ src/tile.rs
@@ -1,6 +1,12 @@
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
+//the indexes inside the enums are for flexibility. You can create an extra array for different
+//types of particular tile type and index it with the index. For example you could have an array of
+//different wall texture variations and have your level generator algorithm put random indexes in
+//the walls so that the level would be more visually varied. You could also also have an array of
+//different types of floors which each have their own properties (normal floor, icy floor, lava floor etc.) 
 pub enum Tile {
-	Wall,
-	Floor,
-	Void,
+	Wall{index: usize},
+	Floor{index: usize},
+	Void{index: usize},
+    Faction{index: usize},
 }
