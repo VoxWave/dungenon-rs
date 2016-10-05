c793de7535f7fc02f4b467c58763c882b8b3b67d
Victor Bankowski <victor_bankowski@hotmail.com>
2016-10-05 21:41:48 +0300

    tiles had tabs and the faction enum which is soon going to be in its own enum

--- src/tile.rs
+++ src/tile.rs
@@ -3,10 +3,9 @@
 //types of particular tile type and index it with the index. For example you could have an array of
 //different wall texture variations and have your level generator algorithm put random indexes in
 //the walls so that the level would be more visually varied. You could also also have an array of
-//different types of floors which each have their own properties (normal floor, icy floor, lava floor etc.) 
+//different types of floors which each have their own properties (normal floor, icy floor, lava floor etc.)
 pub enum Tile {
-	Wall(usize),
-	Floor(usize),
-	Void(usize),
-    Faction(usize),
+    Wall(usize),
+    Floor(usize),
+    Void(usize),
 }
