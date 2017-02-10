444525ec5d47d11b436a03bb66d3ba6f0618c17a
Victor Bankowski <victor_bankowski@hotmail.com>
2017-02-11 00:25:02 +0200

    thing that was sent for delma has been fixed. this will not build though.

--- src/generator/faction.rs
+++ src/generator/faction.rs
@@ -11,43 +11,50 @@ pub struct FactionGen {
 }
 
 impl FactionGen {
-	pub fn new(faction_amount: usize) -> FactionGen {
+    pub fn new(faction_amount: usize) -> FactionGen {
         FactionGen {
             rand: XorShiftRng::rand(&mut OsRng::new().unwrap()),
             faction_amount: faction_amount,
         }
-	}
+    }
 
-	pub fn generate(&mut self, level: &mut Level<Faction>) {
+    pub fn generate(&mut self, level: &mut Level<Faction>) {
         let delta = Vec::new();
-		for x in 0..level.get_width() {
-			for y in 0..level.get_height() {
-				match level.get_mut_tile(x, y) {
-					Ok(tile) => {
-						let mut deck = Vec::new();
-						match *tile {
-							Faction::Faction(_) => {
-								deck.push(tile.clone());
-							},
-							Faction::Void => continue,
-							_ => {},
-						}
-						get_faction_neighbours(&mut deck, &mut level);
-						self.rand.shuffle(&mut deck);
-						delta.push(((x,y),deck.pop()));
-					},
-					Err(Error::IndexOutOfBounds) => {
-						panic!("Generate method indexed out of bounds while simulating a step. This should never happen unless the programmer is not very bright.");
-					}
-				}
-			}
-		}
+        for x in 0..level.get_width() {
+            for y in 0..level.get_height() {
+                match level.get_mut_tile(x, y) {
+                    Ok(tile) => {
+                        let mut deck = Vec::new();
+                        match *tile {
+                            Faction::Faction(_) => {
+                                deck.push(tile.clone());
+                            },
+                            Faction::Void => continue,
+                            _ => {},
+                        }
+                        get_faction_neighbours(&mut deck, &mut level);
+                        self.rand.shuffle(&mut deck);
+                        match deck.pop() {
+                            Some(f) => {
+                                delta.push(((x,y), f));
+                            },
+                            None => continue,
+                        }
+                    },
+                    Err(Error::IndexOutOfBounds) => {
+                        panic!("Generate method indexed out of bounds while simulating a step. This should never happen unless the programmer is not very bright.");
+                    }
+                }
+            }
+        }
         Self::applyChanges(level, delta);
-	}
+    }
 
     fn applyChanges(level: &mut Level<Faction>, delta: Vec<((usize, usize), Faction)>) {
         for ((x, y), f) in delta {
-            level.get_mut_tile(x,y).unwrap()* = f;
+            if let Ok(tile) = level.get_mut_tile(x, y) {
+                *tile = f;
+            }
         }
     }
 }
